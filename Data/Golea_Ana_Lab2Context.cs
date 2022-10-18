864de539592b781bca5e56c46f60ab0cb5ac1bda
Ana <Ana@DESKTOP-SA8MC31>
2022-10-18 17:05:55 +0300

    Add project files.

---
+++ Data/Golea_Ana_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Golea_Ana_Lab2.Models;
+
+namespace Golea_Ana_Lab2.Data
+{
+    public class Golea_Ana_Lab2Context : DbContext
+    {
+        public Golea_Ana_Lab2Context (DbContextOptions<Golea_Ana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Golea_Ana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Golea_Ana_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Golea_Ana_Lab2.Models.Author> Author { get; set; }
+    }
+}
