1eae7e2439b976734f517a1f96bda7964f3f6a88
Ana Maria Golea <ana.m.golea@gmail.com>
2022-11-01 16:25:50 +0200

    rezolvare laborator 4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,21 @@
-﻿namespace Golea_Ana_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Golea_Ana_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
