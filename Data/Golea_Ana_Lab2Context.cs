5177298c9dc28433b79022a51cfbb3dbcbdea655
Ana Maria Golea <ana.m.golea@gmail.com>
2022-11-08 16:45:09 +0200

    rezolvare laborator 5 (nu merge login)

--- Data/Golea_Ana_Lab2Context.cs
+++ Data/Golea_Ana_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Golea_Ana_Lab2.Data
         public DbSet<Golea_Ana_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Golea_Ana_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Golea_Ana_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Golea_Ana_Lab2.Models.Borrowing> Borrowing { get; set; }
     }
 }
